065a812525071b5254e55cae06b609dac579aa41
Denuwan <90548223+ImDenuwan@users.noreply.github.com>
2021-11-01 09:44:32 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,40 +1,57 @@
 import os
 from PIL import Image
 from pyrogram import Client,filters 
-from pyrogram.types import (InlineKeyboardButton,  InlineKeyboardMarkup)
+from pyrogram.types import InlineKeyboardButton,  InlineKeyboardMarkup
+from pyrogram.errors import UserNotParticipant, ChatAdminRequired, UsernameNotOccupied
 
-from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 
 TOKEN = os.environ.get("TOKEN", "")
 
 API_ID = int(os.environ.get("API_ID", 12345))
 
 API_HASH = os.environ.get("API_HASH", "")
+
 app = Client(
         "pdf",
-        bot_token=TOKEN,api_hash=API_HASH,
-            api_id=API_ID
+        bot_token=TOKEN,
+        api_hash=API_HASH,
+        api_id=API_ID
     )
 
 
 LIST = {}
 
-@slbotzone.on_message(filters.command(['start']))
-async def start(client, message):
- await message.reply_text(text =f"""Hello {message.from_user.first_name }image to pdf bot 
+JOIN_ASAP = " **You can't use me untill subscribe our updates channel** ☹️\n\n So Please join our updates channel by the following button and hit on the ` /start ` button again 😊"
 
-i can convert image to pdf
+FSUBB = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton(text="Join our update Channel 🗣", url=f"https://t.me/szteambots") 
+        ]]      
+    )
+
+@app.on_message(filters.command(['start']))
+async def start(client, message):
+    try:
+        await message._client.get_chat_member(int("-1001325914694"), message.from_user.id)
+    except UserNotParticipant:
+        await message.reply_text(
+        text=JOIN_ASAP, disable_web_page_preview=True, reply_markup=FSUBB
+    )
+        return 
+    await message.reply_text(text =f"""Hello {message.from_user.first_name }\n I'm image to pdf bot. 
+i can convert image to pdf\n\n Just send a image
 
 This bot created by @InukaRanmira""",reply_to_message_id = message.message_id ,  reply_markup=InlineKeyboardMarkup(
             [
-                [
-                    InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/slbotzone") ],
-                    [InlineKeyboardButton("Channel", url="https://t.me/szteambots") ],   
+                
+                    [InlineKeyboardButton("Support " ,url="https://t.me/slbotzone") ],
+                    [InlineKeyboardButton("Channel", url="https://t.me/szteambots") ]
+                    ],[
                     [InlineKeyboardButton("Deverloper", url="https://t.me/InukaRanmira") ],  
+            ]))
 
 
-
-@slbotzone.on_message(filters.private & filters.photo)
+@app.on_message(filters.private & filters.photo)
 async def pdf(client,message):
  
  if not isinstance(LIST.get(message.from_user.id), list):
@@ -52,7 +69,7 @@ async def pdf(client,message):
  await ms.edit(f"{len(LIST[message.from_user.id])} image   Successful created PDF if you want add more image Send me One by one\n\n **if done click here 👉 /convert** ")
  
 
-@slbotzone.on_message(filters.command(['convert']))
+@app.on_message(filters.command(['convert']))
 async def done(client,message):
  images = LIST.get(message.from_user.id)
 
@@ -67,8 +84,5 @@ async def done(client,message):
  
  await client.send_document(message.from_user.id, open(path, "rb"), caption = "Here your pdf !!")
  os.remove(path)
- 
- 
- 
- 
-slbotzone.run()
+  
+app.run()
